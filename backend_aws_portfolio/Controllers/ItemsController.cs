56099aa569fde4d76ec60d8ee1ff531bf80420f6
Anh Truong Nguyen <truongnguyen4042@gmail.com>
2023-08-02 21:56:33 +1000

    Add project files.

---
+++ backend_aws_portfolio/Controllers/ItemsController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using dotenv.net;
+
+namespace backend_aws_portfolio.Controllers
+{
+         [ApiController]
+        [Route("api/[controller]")]
+        public class ItemsController : ControllerBase
+        {
+            // GET: api/<ItemsController>
+            [HttpGet]
+            public IEnumerable<string> Get()
+            {
+                return new string[] { "value1", "value2" };
+            }
+
+            // GET api/<ItemsController>/5
+            [HttpGet("pdf")]
+            public IActionResult GetPdf()
+            {
+                DotEnv.Load(); // // Loads the environment variables from .env
+                               //var s3PdfUrl = "https://porfolio-truong.s3.amazonaws.com/Truong_Resume.pdf";
+                string s3PdfUrl = System.Environment.GetEnvironmentVariable("PDF_URL");
+                return Ok(s3PdfUrl);
+            }
+
+            // POST api/<ItemsController>
+            [HttpPost]
+            public void Post([FromBody] string value)
+            {
+            }
+
+            // PUT api/<ItemsController>/5
+            [HttpPut("{id}")]
+            public void Put(int id, [FromBody] string value)
+            {
+            }
+
+            // DELETE api/<ItemsController>/5
+            [HttpDelete("{id}")]
+            public void Delete(int id)
+            {
+            }
+        }
+    
+}
+    
+
---
+++ backend_aws_portfolio/Program.cs
@@ -0,0 +1,26 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddEndpointsApiExplorer();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+   
+}
+
+app.UseAuthorization();
+
+//app.MapControllerRoute(
+    //name: "items",
+    //: "{controller=Items}/{action=api/items/pdf}");
+
+app.MapControllers();
+
+app.Run();
